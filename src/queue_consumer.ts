06333c4ffba44cf92a5f94ff7b5d789c6f2c85bb
ThainanST <66067872+ThainanST@users.noreply.github.com>
2024-06-16 16:46:41 -0300

    adding rabbitMQ

---
+++ src/queue_consumer.ts
@@ -0,0 +1,14 @@
+import amqp from "amqplib";
+
+async function init() {
+    const connection = await amqp.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+    await channel.assertQueue("checkout", { durable: true });
+    await channel.consume("checkout", async function (message: any) {
+        const input = JSON.parse(message.content.toString());
+
+        // TODO: Implement the checkout logic here
+    });
+}
+
+init();

cc4fa7f3f1a54e2d24f8b7f15a7d72b150155994
ThainanST <66067872+ThainanST@users.noreply.github.com>
2024-08-02 16:41:33 -0300

    working frontend test

---
+++ frontend/src/App.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+    const products = reactive([
+        { idProduct: 1, description: 'A', price: 1000 },
+        { idProduct: 2, description: 'B', price: 5000 },
+        { idProduct: 3, description: 'C', price: 30 },
+    ]);
+
+    const order = reactive({
+        total: 0,
+        items: [] as any,
+    })
+
+    const addItem = function (product: any) {
+        order.items.push({
+            idProduct: product.idProduct,
+            price: product.price,
+            quantity: 1,
+        });
+    }
+
+    const getTotal = function () {
+        let total = 0;
+        for (const item of order.items) {
+            total += item.price
+        }
+        return total;
+    }
+
+</script>
+
+<template>
+    <div class="title">Checkout</div>
+    <div v-for="product in products">
+        <span class="product-description">{{ product.description }}</span>
+        <span class="product-price">{{ product.price }}</span>
+        <button class="product-add-button" @click="addItem(product)">add</button>
+    </div>
+    <span class="total">{{ getTotal() }}</span>
+</template>
+
+<style scoped>
+</style>
